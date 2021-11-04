55ee998dde1493fb6c7f1aa1c73b1a137786cb3d
Jake <jakewarrenblack01@gmail.com>
2021-11-04 13:24:24 +0000

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link :to="{name: 'home'}">Home</router-link>|
+    <router-link :to="{name: 'about'}">About</router-link>|
+    <router-link :to="{name: 'contact'}">Contact</router-link>|
+    <router-link :to="{name: 'projects'}">Projects</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
---
+++ src/pages/projects/AllProjects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>All projects</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllProjects',
+  components: {
+    
+  }
+}
+</script>
